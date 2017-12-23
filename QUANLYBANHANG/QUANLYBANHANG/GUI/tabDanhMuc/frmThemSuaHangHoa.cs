11e42a4b6ce2ce4988c54c09de36e943f0020df5
LuanPham <luanpham.997@gmail.com>
2017-12-23 10:26:06 +0700

    xu ly tsmi

---
+++ QUANLYBANHANG/QUANLYBANHANG/GUI/tabDanhMuc/frmThemSuaHangHoa.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QUANLYBANHANG.BUS;
+using QUANLYBANHANG.DAO;
+using System.Data.SqlClient;
+using QUANLYBANHANG.DTO;
+
+namespace QUANLYBANHANG.GUI
+{
+    public partial class frmThemSuaHangHoa : Form
+    {
+        NGHIEPVU_KHO nv_kho = new NGHIEPVU_KHO();
+        NGHIEPVU_NHOMHANG nv_nhomhang = new NGHIEPVU_NHOMHANG();
+        NGHIEPVU_DONVITINH nv_dvt = new NGHIEPVU_DONVITINH();
+        NGHIEPVU_NHACUNGCAP nv_ncc = new NGHIEPVU_NHACUNGCAP();
+        NGHIEPVU_HANGHOA nv_hh = new NGHIEPVU_HANGHOA();
+
+        // lưu trạng thái là đang thêm đang sửa
+        int Cur_State = 1;
+
+        // sự kiện sau khi thêm sửa
+        public delegate void ThayDoiDuLieuHangHoa();
+        public event ThayDoiDuLieuHangHoa KhiThayDoi;
+
+        // dùng để thêm
+        public frmThemSuaHangHoa()
+        {
+            InitializeComponent();
+
+            Cur_State = 1;
+
+            Load += new EventHandler(frmLoad_Them);
+            btnDong.Click += new EventHandler(btnDong_click);
+            btnLuu.Click += new EventHandler(btnLuu_click);
+
+            this.Text = "Thêm Hàng Hóa, Dịch Vụ";
+        }
+
+        // dùng để sửa
+        public frmThemSuaHangHoa(HangHoa hh)
+        {
+            InitializeComponent();
+
+            Cur_State = 0;
+
+            this.Text = "Cập nhật Hàng Hóa, Dịch Vụ";
+            btnDong.Click += new EventHandler(btnDong_click);
+            btnLuu.Click += new EventHandler(btnLuu_click);
+
+            FillDuLieu(hh);
+        }
+
+        private void FillDuLieu(HangHoa hh)
+        {
+            FillCbLoaiHangHoa();
+            FillCbKhoMacDinh();
+            FillCbPhanLoai();
+            FillCbDonVi();
+            FillCbNCC();
+
+            cbLoaiHH.Text = hh.LoaiHangHoa;
+            lkueKhoMacDinh.EditValue = hh.KhoMacDinh;
+            lkuePhanLoai.EditValue = hh.PhanLoai;
+            txtMaHang.Text = hh.MaHangHoa;
+            txtMaHang.ReadOnly = true;
+            txtMaVachNSX.Text = hh.MaVachNSX;
+            txtTenHang.Text = hh.TenHang;
+            lkueDonVi.EditValue = hh.DonVi;
+            txtXuatXu.Text = hh.XuatXu;
+            nmrTonKhoToiThieu.Value = hh.TonKhoToiThieu;
+            nmrTonKhoHienTai.Value = hh.TonHienTai;
+            lkueNCC.EditValue = hh.NCC;
+            nmrGiaMua.Value = hh.GiaMua;
+            nmrGiaBanLe.Value = hh.GiaBanLe;
+            nmrGiaBanSi.Value = hh.GiaBanSi;
+            checkBConQuanLy.Checked = hh.ConQuanLy;
+        }
+
+        private void btnLuu_click(object sender, EventArgs e)
+        {
+            HangHoa hh = new HangHoa()
+            {
+                LoaiHangHoa = cbLoaiHH.Text,
+                KhoMacDinh = lkueKhoMacDinh.EditValue.ToString(),
+                PhanLoai = lkuePhanLoai.EditValue.ToString(),
+                MaHangHoa = txtMaHang.Text,
+                MaVachNSX = txtMaVachNSX.Text,
+                TenHang = txtTenHang.Text,
+                DonVi = lkueDonVi.EditValue.ToString(),
+                XuatXu = txtXuatXu.Text,
+                TonKhoToiThieu = (int)nmrTonKhoToiThieu.Value,
+                TonHienTai = (int)nmrTonKhoHienTai.Value,
+                NCC = lkueNCC.EditValue.ToString(),
+                GiaMua = int.Parse(nmrGiaMua.Value.ToString()),
+                GiaBanLe = int.Parse(nmrGiaBanLe.Value.ToString()),
+                GiaBanSi = int.Parse(nmrGiaBanSi.Value.ToString()),
+                ConQuanLy = checkBConQuanLy.Checked ? true : false
+            };
+
+            if (Cur_State == 1)
+            {
+                int kq = nv_hh.ThemHangHoa(hh);
+                if (kq == 1)
+                {
+                    KhiThayDoi();
+                    MessageBox.Show("Thêm hàng hóa thành công !");
+                }
+                else
+                    MessageBox.Show("Không được để trống các trường dữ liệu quan trọng");
+            }
+
+            else if (Cur_State == 0)
+            {
+                int kq = nv_hh.CapNhatHangHoa(hh);
+                if (kq == 1)
+                {
+                    KhiThayDoi();
+                    MessageBox.Show("Cập nhật hàng hóa thành công !");
+                }
+                else
+                    MessageBox.Show("Không được để trống các trường dữ liệu quan trọng");
+            }
+        }
+
+        private void btnDong_click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmLoad_Them(object sender, EventArgs e)
+        {
+            FillCbLoaiHangHoa();
+            FillCbKhoMacDinh();
+            FillCbPhanLoai();
+            GenerateMa();
+            FillCbDonVi();
+            FillCbNCC();
+
+            nmrTonKhoToiThieu.Value = 0;
+            nmrTonKhoHienTai.Value = 0;
+            nmrGiaMua.Value = 0;
+            nmrGiaBanLe.Value = 0;
+            nmrGiaBanSi.Value = 0;
+            checkBConQuanLy.Checked = true;
+        }
+
+        private void FillCbLoaiHangHoa()
+        {
+            string[] LoaiHangHoa = new string[] { "Hàng Hóa", "Dịch Vụ" };
+            cbLoaiHH.Items.AddRange(LoaiHangHoa);
+            cbLoaiHH.SelectedIndex = 0;
+        }
+
+        private void FillCbKhoMacDinh()
+        {
+            string sql = "select MaKho, TenKho from KHOHANG";
+            DataTable dt = nv_kho.LayDuLieuKho(sql);
+
+            lkueKhoMacDinh.Properties.DataSource = dt;
+            lkueKhoMacDinh.Properties.ValueMember = "MaKho";
+            lkueKhoMacDinh.Properties.DisplayMember = "TenKho";
+            lkueKhoMacDinh.ItemIndex = 0;
+        }
+
+        private void FillCbPhanLoai()
+        {
+            string sql = "select MaNhomHang, TenNhomHang from NHOMHANG";
+            DataTable dt = nv_nhomhang.LayDuLieuNhomHang(sql);
+
+            lkuePhanLoai.Properties.DataSource = dt;
+            lkuePhanLoai.Properties.ValueMember = "MaNhomHang";
+            lkuePhanLoai.Properties.DisplayMember = "TenNhomHang";
+            lkuePhanLoai.ItemIndex = 0;
+        }
+
+        private void GenerateMa()
+        {
+            string mahh = LayMaHangHoa();
+
+            txtMaHang.Text = mahh;
+            txtMaVachNSX.Text = mahh;
+
+            // không cho sửa mã hàng
+            txtMaHang.ReadOnly = true;
+        }
+
+        private string LayMaHangHoa()
+        {
+            string sql = "sp_LayMaHangHoa";
+
+            Provider p = new Provider();
+            p.Connect();
+
+            SqlParameter ma = new SqlParameter("@kq", SqlDbType.VarChar, 10);
+            ma.Direction = ParameterDirection.Output;
+
+            p.ExecuteNonQuery(CommandType.StoredProcedure, sql, ma);
+
+            p.Disconnect();
+
+            return ma.Value.ToString();
+        }
+
+        private void FillCbDonVi()
+        {
+            string sql = "select MaDVTinh, TenDVTinh from DONVITINH";
+            DataTable dt = nv_dvt.LayDuLieuDonViTinh(sql);
+
+            lkueDonVi.Properties.DataSource = dt;
+            lkueDonVi.Properties.ValueMember = "MaDVTinh";
+            lkueDonVi.Properties.DisplayMember = "TenDVTinh";
+            lkueDonVi.ItemIndex = 0;
+        }
+
+        private void FillCbNCC()
+        {
+            string sql = "select MaNCC, TenNCC from NHACUNGCAP";
+            DataTable dt = nv_ncc.LayDuLieuNCC(sql);
+
+            lkueNCC.Properties.DataSource = dt;
+            lkueNCC.Properties.ValueMember = "MaNCC";
+            lkueNCC.Properties.DisplayMember = "TenNCC";
+            lkueNCC.ItemIndex = 0;
+        }
+    }
+}
---
+++ QUANLYBANHANG/QUANLYBANHANG/GUI/tabHeThong/ucPhanQuyen.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QUANLYBANHANG.GUI.HeThong;
+using QUANLYBANHANG.DTO;
+using QUANLYBANHANG.BUS;
+using QUANLYBANHANG.DAO;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using DevExpress.XtraTreeList;
+
+namespace QUANLYBANHANG.GUI
+{
+    public partial class ucPhanQuyen : UserControl
+    {
+        NGHIEPVU_VAITRO nv_vt = new NGHIEPVU_VAITRO();
+        NGHIEPVU_NGUOIDUNG nv_nd = new NGHIEPVU_NGUOIDUNG();
+        NGHIEPVU_VAITRO_CHUCNANG nv_vtcn = new NGHIEPVU_VAITRO_CHUCNANG();
+
+        string MaVaiTro = null;
+        string IDUser = null;
+        int numChildNode = 0;
+        string TenVT = null;
+        string TenDangNhap = null;
+
+        public ucPhanQuyen(VaiTro_ChucNang phanquyenPQ)
+        {
+            InitializeComponent();
+
+            Load += UcPhanQuyen_Load;
+
+            this.ContextMenuStrip = ctmsPhanQuyen;
+
+            // ẩn context menu strip khi click vào header column
+            gvDsUser.MouseUp += gvdsUserMouseUp;
+            tlBPQ.MouseUp += TlBPQ_MouseUp;
+
+            //khu vực định nghĩa sự kiện button
+            btnThemNguoiDung.Click += btnThemNguoiDung_Click;
+            btnThemVaiTro.Click += BtnThemVaiTro_Click;
+            btnSua.Click += BtnSua_Click;
+            btnXoa.Click += BtnXoa_Click;
+
+            //sự kiện click tsmi
+            tsmiThemVT.Click += BtnThemVaiTro_Click;
+            tsmiThemND.Click += btnThemNguoiDung_Click;
+            tsmiSua.Click += BtnSua_Click;
+            tsmiXoa.Click += BtnXoa_Click;
+
+
+
+            tvVaiTroNguoiDung.AfterSelect += TvVaiTroNguoiDung_AfterSelect;
+
+            //phân quyền dựa vào bảng vai trò chức năng đã được gửi qua
+            if (phanquyenPQ != null)
+            {
+                btnThemNguoiDung.Enabled = phanquyenPQ.Them == 0 ? false : true;
+                btnThemVaiTro.Enabled= phanquyenPQ.Them == 0 ? false : true;
+                btnSua.Enabled = phanquyenPQ.Sua == 0 ? false : true;
+                btnXoa.Enabled = phanquyenPQ.Xoa == 0 ? false : true;
+            }
+        }
+
+        private void TlBPQ_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                TreeList view = sender as TreeList;
+                TreeListHitInfo info = view.CalcHitInfo(e.Location);
+                if (info.HitInfoType == HitInfoType.Column)
+                {
+                    this.ContextMenuStrip.Hide();
+                }
+            }
+        }
+
+        private void gvdsUserMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button==MouseButtons.Right)
+            {
+                GridView view = sender as GridView;
+                GridHitInfo info = view.CalcHitInfo(e.Location);
+                if (info.HitTest == GridHitTest.Column)
+                {
+                    this.ContextMenuStrip.Hide();
+                }
+            }
+        }
+
+        private void BtnXoa_Click(object sender, EventArgs e)
+        {
+            if (IDUser == null)
+            {
+                if (numChildNode == 0)
+                {
+                    DialogResult dr = MessageBox.Show(string.Format("Bạn có chắc chắn muốn xóa VAI TRÒ:\n\n\t{0}", TenVT), "Thông báo hệ thống", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dr == DialogResult.Yes)
+                    {
+                        nv_vtcn.XoaVTCN(MaVaiTro);
+                        nv_vt.XoaVaiTro(MaVaiTro);
+                        FillTreeView();
+                        MessageBox.Show("Xóa thành công!");
+                    }
+                }
+                else if (numChildNode > 0)
+                {
+                    MessageBox.Show("Phải xóa hết người dùng thuộc nhóm này trước khi xóa vai trò", "Hệ Thống");
+                }
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show(string.Format("Bạn có chắc chắn muốn xóa NGƯỜI DÙNG:\n\n\t{0}", TenDangNhap), "Thông báo hệ thống", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    nv_nd.XoaNguoiDung(TenDangNhap);
+                    FillTreeView();
+                    MessageBox.Show("Xóa thành công!");
+                }
+            }
+        }
+
+        private void BtnSua_Click(object sender, EventArgs e)
+        {
+            if (IDUser == null)
+            {
+                VaiTro vt = LayVaiTroDangChon();
+                frmThemSuaVaiTro frmvt = new frmThemSuaVaiTro(vt);
+                frmvt.KhiThemThanhCong += FillTreeView;
+                frmvt.ShowDialog();
+            }
+            else
+            {
+                NguoiDung nd = LayNguoiDungDangChon();
+                frmThemSuaNguoiDung frm = new frmThemSuaNguoiDung(nd);
+                frm.KhiThemThanhCong += FillTreeView;
+                frm.ShowDialog();
+            }
+        }
+
+        private VaiTro LayVaiTroDangChon()
+        {
+            string sql = string.Format("select * from VAITRO where MaVaiTro='{0}'", MaVaiTro);
+            DataTable dt = Execute.LayDuLieuBang(sql);
+            DataRow r = dt.Rows[0];
+
+            VaiTro vt = new VaiTro();
+            vt.MaVaiTro = MaVaiTro;
+            vt.TenVaiTro = r["TenVaiTro"].ToString();
+            vt.DienGiai = r["DienGiai"].ToString();
+            vt.KichHoat = bool.Parse(r["KichHoat"].ToString());
+
+            return vt;
+        }
+
+        private NguoiDung LayNguoiDungDangChon()
+        {
+            string sql = "select * from NGUOIDUNG where ID = " + IDUser;
+            DataTable dt = Execute.LayDuLieuBang(sql);
+            DataRow r = dt.Rows[0];
+
+            NguoiDung nd = new NguoiDung();
+            nd.ID = IDUser;
+            nd.TenDangNhap = r["TenDangNhap"].ToString();
+            nd.Password = r["Password"].ToString();
+            nd.MaVaiTro = r["MaVaiTro"].ToString();
+            nd.MaNV = r["MaNV"].ToString();
+            nd.DienGiai = r["DienGiai"].ToString();
+            nd.ConQuanLy = bool.Parse(r["ConQuanLy"].ToString());
+
+            return nd;
+        }
+
+        private void TvVaiTroNguoiDung_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode tn = e.Node;
+
+            numChildNode = tn.GetNodeCount(true);
+
+            if (tn.Parent == null)
+            {
+                VaiTro vt = tn.Tag as VaiTro;
+                MaVaiTro = vt.MaVaiTro;
+                TenVT = vt.TenVaiTro;
+                IDUser = null;
+                TenDangNhap = null;
+            }
+            else
+            {
+                NguoiDung nd = tn.Tag as NguoiDung;
+                MaVaiTro = nd.MaVaiTro;
+                IDUser = nd.ID;
+                TenDangNhap = nd.TenDangNhap;
+            }
+
+            FillGridView(MaVaiTro);
+
+            FillTreeList(MaVaiTro);
+        }
+
+        private void FillTreeList(string maVaiTro)
+        {
+            string sql = string.Format("select vc.MaChucNang, cn.TenChucNang, vc.TatCa, vc.TruyCap, vc.Them, vc.Xoa, vc.Sua, vc.InAn, vc.Nhap, vc.Xuat, cn.MaCha" +
+                                    " from VAITRO_CHUCNANG vc, CHUCNANG cn" +
+                                    " where vc.MaChucNang = cn.MaChucNang and vc.MaVaiTro = '{0}'", maVaiTro);
+            DataTable dt = Execute.LayDuLieuBang(sql);
+
+            tlBPQ.DataMember = null;
+            tlBPQ.DataSource = dt;
+            tlBPQ.KeyFieldName = "MaChucNang";
+            tlBPQ.ParentFieldName = "MaCha";
+        }
+
+        private void FillGridView(string mavt)
+        {
+            string sql = "select nd.TenDangNhap, vt.TenVaiTro, nd.DienGiai, nd.ConQuanLy" +
+                            " from NGUOIDUNG nd, VAITRO vt" +
+                            " where nd.MaVaiTro = vt.MaVaiTro" +
+                            string.Format(" and nd.MaVaiTro = '{0}'", mavt);
+
+            DataTable dt = Execute.LayDuLieuBang(sql);
+            gcdsUser.DataSource = dt;
+        }
+
+        private void UcPhanQuyen_Load(object sender, EventArgs e)
+        {
+            FillTreeView();
+        }
+
+        private void FillTreeView()
+        {
+            tvVaiTroNguoiDung.Nodes.Clear();
+
+            List<VaiTro> dsVaiTro = LayListVaiTro();
+
+            foreach(VaiTro vt in dsVaiTro)
+            {
+                TreeNode tn = new TreeNode(vt.TenVaiTro);
+                tn.Tag = vt;
+                tvVaiTroNguoiDung.Nodes.Add(tn);
+                tvVaiTroNguoiDung.TopNode = tn;
+
+                FillChildNode(vt.MaVaiTro, tn);
+            }
+
+            tvVaiTroNguoiDung.ExpandAll();
+        }
+
+        private void FillChildNode(string mavt, TreeNode topnode)
+        {
+            DataTable dt = nv_nd.LayDuLieuNguoiDung(string.Format("select ID, TenDangNhap, MaVaiTro from NGUOIDUNG where MaVaiTro = '{0}'", mavt));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                NguoiDung nd = new NguoiDung();
+
+                nd.ID = row["ID"].ToString();
+                nd.TenDangNhap = row["TenDangNhap"].ToString();
+                nd.MaVaiTro = row["MaVaiTro"].ToString();
+
+                TreeNode tn = new TreeNode(nd.TenDangNhap);
+                tn.Tag = nd;
+
+                topnode.Nodes.Add(tn);
+            }
+        }
+
+        private List<VaiTro> LayListVaiTro()
+        {
+            DataTable dt = nv_vt.LayDuLieuVaiTro("select * from VAITRO");
+            List<VaiTro> lvt = new List<VaiTro>();
+
+            foreach(DataRow row in dt.Rows)
+            {
+                VaiTro vt = new VaiTro();
+                vt.MaVaiTro = row["MaVaiTro"].ToString();
+                vt.TenVaiTro = row["TenVaiTro"].ToString();
+                vt.DienGiai= row["DienGiai"].ToString();
+                vt.KichHoat = bool.Parse(row["KichHoat"].ToString());
+
+                lvt.Add(vt);
+            }
+
+            return lvt;
+        }
+
+        private void BtnThemVaiTro_Click(object sender, EventArgs e)
+        {
+            frmThemSuaVaiTro frmtvt = new frmThemSuaVaiTro();
+            frmtvt.ShowDialog();
+        }
+
+        private void btnThemNguoiDung_Click(object sender, EventArgs e)
+        {
+            frmThemSuaNguoiDung frmnd = new frmThemSuaNguoiDung();
+            frmnd.KhiThemThanhCong += FillTreeView;
+            frmnd.ShowDialog();
+        }
+    }
+}
